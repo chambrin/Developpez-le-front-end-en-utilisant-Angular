67273a74993fa895f044a23d7dddf8bc7b1479a6
chambrin <achambrin@outlook.fr>
2024-11-20 11:02:14 +0100

    ⚡️ Olympics service

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -2,19 +2,20 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import {Olympic} from "../models/Olympic";
 
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
   private olympicUrl = './assets/mock/olympic.json';
-  private olympics$ = new BehaviorSubject<any>(undefined);
+  private olympics$ = new BehaviorSubject<Olympic[] | null>(null);
   private error$ = new BehaviorSubject<string | null>(null);
 
   constructor(private http: HttpClient) {}
 
   loadInitialData() {
-    return this.http.get<any>(this.olympicUrl).pipe(
+    return this.http.get<Olympic[]>(this.olympicUrl).pipe(
       tap((value) => {
         this.olympics$.next(value);
         this.error$.next(null);
@@ -23,10 +24,8 @@ export class OlympicService {
         let errorMessage = 'Une erreur est survenue';
 
         if (error.error instanceof ErrorEvent) {
-          // Erreur côté client
           errorMessage = `Erreur: ${error.error.message}`;
         } else {
-          // Erreur côté serveur
           errorMessage = `Code d'erreur: ${error.status}\nMessage: ${error.message}`;
         }
 
@@ -37,7 +36,7 @@ export class OlympicService {
     );
   }
 
-  getOlympics() {
+  getOlympics(): Observable<Olympic[] | null> {
     return this.olympics$.asObservable();
   }
 
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { OlympicService } from 'src/app/core/services/olympic.service';
+import { Observable } from 'rxjs';
+import {Olympic} from "../../core/models/Olympic";
+import {OlympicService} from "../../core/services/olympic.service";
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
+  olympics$!: Observable<Olympic[] | null>;
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+    this.olympicService.loadInitialData().subscribe({
+      error: (error) => {
+        console.error('Erreur lors du chargement des données:', error);
+      }
+    });
+  }
+
+  getTotalMedals(country: Olympic): number {
+    return country.participations.reduce((total, participation) =>
+      total + participation.medalsCount, 0);
   }
 }
